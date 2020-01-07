f29b75ba4bc27cbe4cfe7c354834ba87bdea4ed0
Leo <55296317+leoramos1292@users.noreply.github.com>
2020-01-07 12:48:24 -0800

    add database

--- Car_Insurance_Project/Car_Insurance_Project/Controllers/HomeController.cs
+++ Car_Insurance_Project/Car_Insurance_Project/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Car_Insurance_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,43 @@ namespace Car_Insurance_Project.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Apply(string FirstName, string LastName, int Age, 
+                                  string EmailAddress, int Car_Year, string Make,
+                                   string Model, bool Dui, int Tickets,
+                                   bool Full_Coverage, bool Liability)
+        {
+            if (string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName) || string.IsNullOrEmpty(EmailAddress) || string.IsNullOrEmpty(Make) || string.IsNullOrEmpty(Model))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+
+            else
+            {
+                using (Car_InsuranceEntities db = new Car_InsuranceEntities())
+                    {
+                    var applicant = new Application
+                    {
+                        FirstName = FirstName,
+                        LastName = LastName,
+                        Age = Age,
+                        EmailAddress = EmailAddress,
+                        Car_Year = Car_Year,
+                        Make = Make,
+                        Model = Model,
+                        Dui = Dui,
+                        Tickets = Tickets,
+                        Full_Coverage = Full_Coverage,
+                        Liability = Liability
+                    };
+
+                    db.Applications.Add(applicant);
+                        db.SaveChanges();
+                    }
+            }
+            return View("Contact");
         }
 
         public ActionResult About()
