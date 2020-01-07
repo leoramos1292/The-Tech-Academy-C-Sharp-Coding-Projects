46d2863b8644cd14a8e3658918ec8769e18c7b3a
Leo <55296317+leoramos1292@users.noreply.github.com>
2020-01-07 10:10:35 -0800

    add car insurance project

---
+++ Car_Insurance_Project/Car_Insurance_Project/Controllers/FormController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Car_Insurance_Project.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Application_2()
+        {
+            return View();
+        }
+    }
+}
